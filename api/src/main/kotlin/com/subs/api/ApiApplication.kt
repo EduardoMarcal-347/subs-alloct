26e554687eaf1d47f9f31161083b2ecfb67f51f6
Eduardo Marçal <edumsfilho347@gmail.com>
2025-08-05 17:32:06 -0300

    intilzing spring project

---
+++ api/src/main/kotlin/com/subs/api/ApiApplication.kt
@@ -0,0 +1,13 @@
+package com.subs.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@SpringBootApplication
+class ApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiApplication>(*args)
+}
---
+++ api/src/test/kotlin/com/subs/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.subs.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
